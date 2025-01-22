610193df4a2dbc946ab77bbdf11f40bbcbc39939
rborralhosilva <rborralhosilva@gmail.com>
2025-01-22 11:54:28 +0100

    Revert "Fork Sync: Update from parent repository"

--- bin/check_node.sh
+++ bin/check_node.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-echo "Checking if the PM2 process 'www' is running..."
+echo "Checking if any Node.js process is running..."
 
-# Ensure PM2 is in the PATH
-PATH=$PATH:/home/host797681/.npm-global/bin
+# Check if any Node.js process is running
+ps cax | grep node > /dev/null
 
-# Get the status of the PM2 process 'www'
-status=$(pm2 show www | grep status | awk '{print $4}')
-
-# Check if the status is 'online'
-if [ "$status" == "online" ]; then
-  echo "PM2 process 'www' is running."
+# Check if the process is running
+if [ $? -eq 0 ]; then
+  echo "Process is running."
 else
-  echo "PM2 process 'www' is not running."
+  echo "Process is not running."
+  # Ensure PM2 is in the PATH
+  PATH=$PATH:/usr/local/bin
   # Start the process using PM2
-  echo "Starting 'www' process with PM2..."
-  pm2 resurrect
+  echo "Starting process with PM2..."
   pm2 start www
 fi
